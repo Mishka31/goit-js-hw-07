3c7aa81dc58903a084e6b657bd2857848190559a
Misha Krasnonos <muzishutka7@gmail.com>
2021-05-29 15:05:39 +0300

    Доделал 1 ДЗ

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,11 @@
 const catigoreisEl = document.querySelectorAll("li.item");
-// console.log(catigoreisEl);
 console.log(`В списке ${catigoreisEl.length} категории.`);
+
+for (let i = 0; i < catigoreisEl.length; i++) {
+  const element = catigoreisEl[i];
+  const firstItemEl = element;
+  const firstItemH2El = firstItemEl.querySelector("h2");
+  console.log(`Категория: ${firstItemH2El.textContent}`);
+  const ulOfFirstItem = firstItemEl.querySelectorAll("li");
+  console.log(`Количество элементов: ${ulOfFirstItem.length}`);
+}
---
+++ src/js/task-02.js
@@ -0,0 +1,14 @@
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+const items = ingredients.map((item) => {
+  const liEl = document.createElement("li");
+  liEL. = item;
+});
+console.log(items);
