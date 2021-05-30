808ccf316eb8815c9674c1cb0104659edd01ea96
Misha Krasnonos <muzishutka7@gmail.com>
2021-05-30 23:51:42 +0300

    4 Completed

---
+++ src/js/task-04.js
@@ -0,0 +1,27 @@
+const counterValue = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
+};
+
+const refs = {
+  spanEl: document.querySelector("#value"),
+  increment: document.querySelector('[data-action="increment"]'),
+  decrement: document.querySelector('[data-action="decrement"]'),
+};
+refs.increment.addEventListener("click", onButtonIncrement);
+refs.decrement.addEventListener("click", onButtonDecrement);
+
+function onButtonIncrement() {
+  counterValue.increment();
+  refs.spanEl.textContent = counterValue.value;
+}
+
+function onButtonDecrement(event) {
+  counterValue.decrement();
+  refs.spanEl.textContent = counterValue.value;
+}
